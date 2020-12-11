b57c572a58ba7c0a504570513d05799ab29ccb3d
Bernd Verst <bverst@gmail.com>
2020-12-11 02:10:38 -0800

    Add Kubeflow pipeline definition

--- src/dataprep/dataprep.py
+++ src/dataprep/dataprep.py
@@ -1,7 +1,5 @@
 import os
-import matplotlib.pyplot as plt
-
-import tensorflow as tf
 
 # Nothing here at the moment
 # NOOP
+print("Processing data completed.")
---
+++ src/kubeflow-pipeline.py
@@ -0,0 +1,36 @@
+import kfp.dsl as dsl
+from kubernetes.client.models import V1EnvVarSource, V1SecretKeySelector
+
+@dsl.pipeline(
+    name='Iris Classifcation',
+    description='Iris Classification Problem'
+)
+def iris_classifier_train(imagetag='latest'):
+
+    operations = {}
+
+    # preprocess data
+    operations['prepdata'] = dsl.ContainerOp(
+        name='preprocess',
+        image='kitchenregistry.azurecr.io/prepdata:' + str(imagetag)
+    )
+
+    # train
+    operations['training'] = dsl.ContainerOp(
+        name='training',
+        image='kitchenregistry.azurecr.io/train:' + str(imagetag)
+    )
+    operations['training'].after(operations['prepdata']).container.add_env_from(
+        V1EnvVarSource(
+            secret_key_ref=V1SecretKeySelector(
+                name='github-access-token',
+                key='GITHUB_ACCESS_TOKEN'))) # Needs to be a kubernetes secret in our namespace
+
+
+    for _, op in operations.items():
+        op.container.set_image_pull_policy("Always")
+
+
+if __name__ == '__main__':
+   import kfp.compiler as compiler
+   compiler.Compiler().compile(iris_classifier_train, __file__ + '.tar.gz')
